eeb095339c3a76bfb74e651c7cff4801d572e32a
Bester Visagie <bester.visagie@gmail.com>
2021-09-12 21:39:33 +0200

    Opening and closing posts.

--- javascript/handleInput.js
+++ javascript/handleInput.js
@@ -1,10 +1,30 @@
 
+const body = document.getElementsByTagName("body")[0];
 const posts = document.getElementsByClassName("post");
 
 for (let i = 0; i < posts.length; i++) {
     const element = posts[i];
-    element.addEventListener("click", function() {
-        element.classList.toggle("post-expanded");
-        console.log(element.classList);
+    element.addEventListener("click", function(e) {
+        if (e.target.className !== "post-close-button") {
+            if (!element.classList.contains("post-expanded")) {
+                element.classList.add("post-expanded");
+            }
+        }
     });    
+}
+
+body.addEventListener("click", function(e) {
+    if(e.target.tagName === 'BODY') {
+        const element = document.getElementsByClassName("post-expanded")[0];
+        if (element !== undefined) {
+            element.classList.remove("post-expanded");
+            element.children[1].scrollTop = 0;
+        }
+    }
+});
+
+function closeButtonClick(e) {
+    const element = document.getElementsByClassName("post-expanded")[0];
+    element.classList.remove("post-expanded");
+    element.children[1].scrollTop = 0;
 }
